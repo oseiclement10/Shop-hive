a7e168c2a2b4aedd874414927e20ea6e9a504cab
Clement Osei Asante <clementoseiasante.nss@gmail.com>
2024-07-17 23:39:57 +0000

    landing setup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -92,37 +92,63 @@
                 </div>
             </div>
         </div>
-        <div class="h-[14px] bg-amber-300 "></div>
+        <div class=" text-sm pt-[2px] pb-[3px]  text-slate-900 font-semibold bg-amber-400 text-center">
+            Are you a university or high school student?, expore our 35% discount option <span class="font-semibold text-white underline">here </span>
+        </div>
     </header>
 
-    <div class="relative isolate px-6 pt-14 lg:px-8">
+    <div class="relative isolate  pt-14 ">
         <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
             aria-hidden="true">
             <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-amber-100 to-emerald-300 opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
                 style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
             </div>
         </div>
-        <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-            <div class="hidden sm:mb-8 sm:flex sm:justify-center">
-                <div
-                    class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                    Announcing our next round of funding. <a href="#"
-                        class="font-semibold text-emerald-600"><span class="absolute inset-0"
-                            aria-hidden="true"></span>Read more <span aria-hidden="true">&rarr;</span></a>
+        <div class="   grid grid-cols-2  w-[90%] mx-auto place-items-center">
+            <div class="mx-auto  py-14 sm:py-24 lg:py-32">
+                <div class=" mb-8">
+                    <h1 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl"> Buy Quality & Affordable
+                        Products </h1>
+                    <p class="mt-6 text-lg leading-8 text-gray-600">Anim aute id magna aliqua ad ad non deserunt sunt.
+                        Qui
+                        irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+                    <div class="mt-10 flex items-center gap-x-6">
+                        <a href="#"
+                            class="rounded-md bg-amber-400 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get
+                            started</a>
+                        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span
+                                aria-hidden="true">→</span></a>
+                    </div>
+                </div>
+                <div class="hidden sm:mb-8 sm:flex ">
+                    <div
+                        class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                        Announcing our next round of discounts. <a href="#"
+                            class="font-semibold text-emerald-800"><span class="absolute inset-0"
+                                aria-hidden="true"></span>Read more <span aria-hidden="true">&rarr;</span></a>
+                    </div>
                 </div>
             </div>
-            <div class="text-center">
-                <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Data to enrich your online
-                    business</h1>
-                <p class="mt-6 text-lg leading-8 text-gray-600">Anim aute id magna aliqua ad ad non deserunt sunt. Qui
-                    irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
-                <div class="mt-10 flex items-center justify-center gap-x-6">
-                    <a href="#"
-                        class="rounded-md bg-emerald-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get
-                        started</a>
-                    <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span
-                            aria-hidden="true">→</span></a>
-                </div>
+            <div class="">
+                <img src="{{Vite::asset('resources/images/cart.png')}}" />
+            </div>
+        </div> 
+        <div class="bg-emerald-600 text-white flex items-center justify-between space-x-8 py-6 px-32">
+            <div class="flex flex-col items-center ">
+                <h2 class="text-2xl font-bold">980</h2>
+                <p class="text-slate-100">Domi redo</p>
+            </div>
+            <div class="flex flex-col items-center ">
+                <h2 class="text-2xl font-bold">30,900 + </h2>
+                <p class="text-slate-100">Domi redo</p>
+            </div>
+            <div class="flex flex-col items-center ">
+                <h2 class="text-2xl font-bold">6,000</h2>
+                <p class="text-slate-100">Menfliki</p>
+            </div>
+            <div class="flex flex-col items-center ">
+                <h2 class="text-2xl font-bold">244</h2>
+                <p class="text-slate-100">Asongoli</p>
             </div>
         </div>
         <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
