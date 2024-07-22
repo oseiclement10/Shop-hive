74e4511ea264fcef19d62a3764b416380b7505c9
Clement Osei Asante <clementoseiasante.nss@gmail.com>
2024-07-22 23:35:04 +0000

    stock setup

--- database/migrations/2024_07_22_232130_create_product_models_table.php
+++ database/migrations/2024_07_22_232130_create_product_models_table.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Models\CategoryModel;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,6 +14,11 @@ return new class extends Migration
         Schema::create('product_models', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->slug('slug');
+            $table->foreignIdFor(CategoryModel::class);
         });
     }
 
