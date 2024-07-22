aec4d160624c85a44880bd429cac5f8f4ab0e89f
Clement Osei Asante <clementoseiasante.nss@gmail.com>
2024-07-22 22:55:16 +0000

    shop card design inp

---
+++ resources/views/components/shop/product-card.blade.php
@@ -0,0 +1,9 @@
+@props(['product'])
+
+<div class="bg-slate-50 px-6 pb-10 pt-4 rounded-md">
+    <p class="bg-red-500 text-white px-2 py-[2px] w-fit rounded-sm text-xs">Out of Stock</p>
+    <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="test title" class="h-40 w-full object-contain" />
+    <h3 class="mt-4 leading-tight w-5/6 text-lg font-semibold text-gray-900">Avalanche Pishon Roundneck T Shirt 100% Cotton </h3>
+    <p class=" mb-4 text-sm text-gray-700"> Black Cotton T shirt <i class="fi fi-rs-heart "></i></p>
+    <p class="mt-1 text-xl text-gray-900 font-bold">GH₵ 98.77</p>
+</div>
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -1,3 +1,9 @@
 <x-layout>
-    <h2>Shop</h2>
+    <header> </header>
+    <section class="grid grid-cols-4 gap-8 px-8">
+        <x-shop.product-card />
+        <x-shop.product-card />
+        <x-shop.product-card />
+        <x-shop.product-card />
+    </section>
 </x-layout>
