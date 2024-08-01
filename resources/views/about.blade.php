16bafdb6ab33ecda4aafa3b77fc71d1382389db2
Clement Osei Asante <clementoseiasante.nss@gmail.com>
2024-08-01 15:33:46 +0000

    setup about and join us pages

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,72 +1,169 @@
 <x-layouts.index>
     <x-layouts.nav />
-    <section class="py-10 bg-gray-100 sm:py-16 lg:py-24">
-        <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="max-w-2xl mx-auto text-center">
-                <h2 class="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">How do we create success
+
+    {{-- OUR TEAM --}}
+
+    <section class="py-10 bg-white sm:py-16 lg:py-24">
+        <div class="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+            <div class="max-w-3xl mx-auto text-center">
+                <h2 class="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl lg:leading-tight">Our Team
                 </h2>
-                <p class="max-w-lg mx-auto mt-4 text-base leading-relaxed text-gray-600">Amet minim mollit non deserunt
+                <p class="max-w-2xl mx-auto mt-4 text-xl text-gray-600">Amet minim mollit non deserunt ullamco est sit
+                    aliqua dolor do amet sint. Velit officia consequat duis.</p>
+            </div>
+
+            <div class="grid grid-cols-1 gap-6 mt-8 sm:grid-cols-3 md:mt-16 lg:gap-x-12">
+                <div>
+                    <img class="w-full"
+                        src="https://cdn.rareblocks.xyz/collection/celebration/images/team/6/team-member-1.jpg"
+                        alt="" />
+                </div>
+
+                <div>
+                    <img class="w-full"
+                        src="https://cdn.rareblocks.xyz/collection/celebration/images/team/6/team-member-2.jpg"
+                        alt="" />
+                </div>
+
+                <div>
+                    <img class="w-full"
+                        src="https://cdn.rareblocks.xyz/collection/celebration/images/team/6/team-member-3.jpg"
+                        alt="" />
+                </div>
+            </div>
+
+            <div class="mt-8 text-center md:mt-16">
+                <a href="#" title=""
+                    class="inline-flex items-center justify-center py-4 font-semibold text-white transition-all duration-200 bg-emerald-600 border border-transparent rounded-md px-14 hover:bg-emerald-700 focus:bg-emerald-700"
+                    role="button"> Join our team </a>
+            </div>
+        </div>
+    </section>
+
+
+    {{-- CONTACT US --}}
+    <section class="py-10 bg-slate-50 sm:py-16 lg:py-24">
+        <div class="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+            <div class="max-w-2xl mx-auto text-center">
+                <h2 class="text-3xl font-bold leading-tight text-gray-900 sm:text-4xl lg:text-5xl">Contact us</h2>
+                <p class="max-w-xl mx-auto mt-4 text-base leading-relaxed text-gray-500">Amet minim mollit non deserunt
                     ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis.</p>
             </div>
 
-            <ul class="max-w-md mx-auto mt-16 space-y-12">
-                <li class="relative flex items-start">
-                    <div class="-ml-0.5 absolute mt-0.5 top-14 left-8 w-px border-l-4 border-dotted border-gray-300 h-full"
-                        aria-hidden="true"></div>
+            <div class="max-w-5xl mx-auto mt-12 sm:mt-16">
+                <div class="grid grid-cols-1 gap-6 px-8 text-center md:px-0 md:grid-cols-3">
+                    <div class="overflow-hidden bg-white rounded-xl">
+                        <div class="p-6">
+                            <svg class="flex-shrink-0 w-10 h-10 mx-auto text-gray-400"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1"
+                                    d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+                            </svg>
+                            <p class="mt-6 text-lg font-medium text-gray-900">+1-316-555-0116</p>
+                            <p class="mt-1 text-lg font-medium text-gray-900">+1-446-526-0117</p>
+                        </div>
+                    </div>
 
-                    <div
-                        class="relative flex items-center justify-center flex-shrink-0 w-16 h-16 bg-white rounded-full shadow">
-                        <svg class="w-10 h-10 text-emerald-600" xmlns="http://www.w3.org/2000/svg" fill="none"
-                            viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1"
-                                d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                        </svg>
+                    <div class="overflow-hidden bg-white rounded-xl">
+                        <div class="p-6">
+                            <svg class="flex-shrink-0 w-10 h-10 mx-auto text-gray-400"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1"
+                                    d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                            </svg>
+                            <p class="mt-6 text-lg font-medium text-gray-900">contact@example.com</p>
+                            <p class="mt-1 text-lg font-medium text-gray-900">hr@example.com</p>
+                        </div>
                     </div>
-                    <div class="ml-6">
-                        <h3 class="text-lg font-semibold text-black">Create a free account</h3>
-                        <p class="mt-4 text-base text-gray-600">Amet minim mollit non deserunt ullamco est sit aliqua
-                            dolor
-                            do amet sint. Velit officia consequat duis enim velit mollit.</p>
-                    </div>
-                </li>
 
-                <li class="relative flex items-start">
-                    <div class="-ml-0.5 absolute mt-0.5 top-14 left-8 w-px border-l-4 border-dotted border-gray-300 h-full"
-                        aria-hidden="true"></div>
+                    <div class="overflow-hidden bg-white rounded-xl">
+                        <div class="p-6">
+                            <svg class="flex-shrink-0 w-10 h-10 mx-auto text-gray-400"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1"
+                                    d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1"
+                                    d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+                            </svg>
+                            <p class="mt-6 text-lg font-medium leading-relaxed text-gray-900">8502 Preston Rd. Ingle,
+                                Maine 98380, USA</p>
+                        </div>
+                    </div>
+                </div>
 
-                    <div
-                        class="relative flex items-center justify-center flex-shrink-0 w-16 h-16 bg-white rounded-full shadow">
-                        <svg class="w-10 h-10 text-emerald-600" xmlns="http://www.w3.org/2000/svg" fill="none"
-                            viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1"
-                                d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122" />
-                        </svg>
-                    </div>
-                    <div class="ml-6">
-                        <h3 class="text-lg font-semibold text-black">Build your website</h3>
-                        <p class="mt-4 text-base text-gray-600">Amet minim mollit non deserunt ullamco est sit aliqua
-                            dolor
-                            do amet sint. Velit officia consequat duis enim velit mollit.</p>
-                    </div>
-                </li>
+                <div class="mt-6 overflow-hidden bg-white rounded-xl">
+                    <div class="px-6 py-12 sm:p-12">
+                        <h3 class="text-3xl font-semibold text-center text-gray-900">Send us a message</h3>
 
-                <li class="relative flex items-start">
-                    <div
-                        class="relative flex items-center justify-center flex-shrink-0 w-16 h-16 bg-white rounded-full shadow">
-                        <svg class="w-10 h-10 text-emerald-600" xmlns="http://www.w3.org/2000/svg" fill="none"
-                            viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1"
-                                d="M13 10V3L4 14h7v7l9-11h-7z" />
-                        </svg>
+                        <form action="#" method="POST" class="mt-14">
+                            <div class="grid grid-cols-1 sm:grid-cols-2 gap-x-5 gap-y-4">
+                                <div>
+                                    <label for="" class="text-base font-medium text-gray-900"> Your name
+                                    </label>
+                                    <div class="mt-2.5 relative">
+                                        <input type="text" name="" id=""
+                                            placeholder="Enter your full name"
+                                            class="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600" />
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <label for="" class="text-base font-medium text-gray-900"> Email address
+                                    </label>
+                                    <div class="mt-2.5 relative">
+                                        <input type="email" name="" id=""
+                                            placeholder="Enter your full name"
+                                            class="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600" />
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <label for="" class="text-base font-medium text-gray-900"> Phone number
+                                    </label>
+                                    <div class="mt-2.5 relative">
+                                        <input type="tel" name="" id=""
+                                            placeholder="Enter your full name"
+                                            class="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600" />
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <label for="" class="text-base font-medium text-gray-900"> Company name
+                                    </label>
+                                    <div class="mt-2.5 relative">
+                                        <input type="text" name="" id=""
+                                            placeholder="Enter your full name"
+                                            class="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600" />
+                                    </div>
+                                </div>
+
+                                <div class="sm:col-span-2">
+                                    <label for="" class="text-base font-medium text-gray-900"> Message
+                                    </label>
+                                    <div class="mt-2.5 relative">
+                                        <textarea name="" id="" placeholder=""
+                                            class="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md resize-y focus:outline-none focus:border-blue-600 caret-blue-600"
+                                            rows="4"></textarea>
+                                    </div>
+                                </div>
+
+                                <div class="sm:col-span-2">
+                                    <button type="submit"
+                                        class="inline-flex items-center justify-center w-full px-4 py-4 mt-2 text-base font-semibold text-white transition-all duration-200 bg-emerald-600 border border-transparent rounded-md focus:outline-none hover:bg-emerald-700 focus:bg-emerald-700">
+                                        Send
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
-                    <div class="ml-6">
-                        <h3 class="text-lg font-semibold text-black">Release & launch</h3>
-                        <p class="mt-4 text-base text-gray-600">Amet minim mollit non deserunt ullamco est sit aliqua
-                            dolor
-                            do amet sint. Velit officia consequat duis enim velit mollit.</p>
-                    </div>
-                </li>
-            </ul>
+                </div>
+            </div>
         </div>
     </section>
+
+    <x-layouts.footer />
+
 </x-layouts.index>
