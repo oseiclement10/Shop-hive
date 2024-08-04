6601e88612a73442eba3c73279ce72c68f82ad21
Clement Osei Asante <clementoseiasante.nss@gmail.com>
2024-08-04 22:59:35 +0000

    added dmsans font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ["./resources/**/*.blade.php", "./resources/**/*.js"],
+    theme: {
+        extend: {
+            fontFamily: {
+                dmsans: ["DM Sans", "sans-serif"],
+                roboto: ["Roboto", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
+};
