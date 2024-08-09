5af90c5d30a5c4d9c51ecd1ed2c2914d0b65a1d5
Clement Osei Asante <clementoseiasante.nss@gmail.com>
2024-08-09 23:34:16 +0000

    replaced normal controllers with livewire components

--- app/Livewire/VendorOrders.php
+++ app/Livewire/VendorOrders.php
@@ -3,11 +3,16 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\OrderItems;
+use Illuminate\Support\Facades\Auth;
 
 class VendorOrders extends Component
 {
+
     public function render()
     {
-        return view('livewire.vendor-orders');
+        return view('livewire.vendor-orders', [
+            "orderItems" => OrderItems::query()->whereIn("product_id", Auth::guard("vendor")->user()->products()->pluck("id"))->get()
+        ]);
     }
 }
--- app/Livewire/VendorProducts.php
+++ app/Livewire/VendorProducts.php
@@ -3,11 +3,14 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Auth;
 
 class VendorProducts extends Component
 {
     public function render()
     {
-        return view('livewire.vendor-products');
+        return view('livewire.vendor-products', [
+            'products' =>  Auth::guard("vendor")->user()->products()->latest()->get(),
+        ]);
     }
 }
