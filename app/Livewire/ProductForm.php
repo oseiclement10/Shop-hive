3725a87920b6deaa2fb3b843c3e2cb8d44a1f0e2
Clement Osei Asante <clementoseiasante.nss@gmail.com>
2024-08-11 11:11:04 +0000

    refactor products form

---
+++ app/Livewire/ProductForm.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+use Livewire\WithFileUploads;
+use Livewire\Attributes\Rule;
+use App\Models\Category;
+use App\Models\Product;
+use Auth;
+
+class ProductForm extends Component
+{
+    use WithFileUploads;
+
+    #[Rule("required", message: "name is required")]
+    public $name;
+
+    #[Rule("required", message: "select at least one category")]
+    public $category = [];
+
+    #[Rule("required", message: "Short description is required")]
+    public $short_description;
+
+    #[Rule("required", message: "Long description is required")]
+    public $long_description;
+
+    #[Rule("required", message: "image is required")]
+    public $img;
+
+    #[Rule("required", message: "price is required")]
+    public $price;
+
+    #[Rule("required", message: "quantity is required")]
+    public $quantity;
+
+    public function save()
+    {
+        $this->validate();
+
+        $imgPath = $this->img->store('products', 'public');
+
+        $product = Product::create([
+            "name" => $this->name,
+            "img" => $imgPath,
+            "vendor_id" => Auth::guard("vendor")->user()->id,
+            "short_description" => $this->short_description,
+            "long_description" => $this->long_description,
+        ]);
+
+        $product->stock()->create([
+            "price" => $this->price,
+            "quantity" => $this->quantity,
+        ]);
+
+
+        $product->categories()->attach($this->category);
+        $this->reset();
+
+        $this->;
+    }
+
+
+    public function render()
+    {
+        return view('livewire.product-form', [
+            'categories' => Category::get()->push((object) ['id' => "0", "name" => "Other"]),
+        ]);
+    }
+}
---
+++ resources/views/livewire/product-form.blade.php
@@ -0,0 +1,33 @@
+<x-form class="space-y-4" wire:submit='save'>
+    <x-input label="Product Name" wire:model="name" class="border-emerald-500" placeholder="name goes here" />
+    <x-file label="Product Image" wire:model="img" accept="image/png, image/jpeg">
+        <img src="{{ Vite::asset('resources/images/upload-img.jpg') }}" alt="product image"
+            class="h-24 rounded-lg" />
+    </x-file>
+
+    <x-input label="Short Description" wire:model="short_description" class="border-emerald-500" placeholder="description goes here" />
+    <x-textarea label="Long Description" wire:model="long_description" placeholder="Product Description ..."
+        rows="5" class="border-emerald-500" />
+
+    <x-choices-offline label="Category" wire:model="category" :options="$categories"
+        hint="select at least one category" class="border-emerald-500"
+        no-result-text="Ops! Nothing here ..." searchable  />
+
+
+    <div class="border-t">
+        <p class="py-2 ml-1 text-sm text-gray-900">Product Stock </p>
+        <div class="grid grid-cols-2 gap-6">
+            <x-input label="Price"  prefix="Gh₵" wire:model="price" class="border-emerald-500" />
+            <x-input label="Quantity" type="number" wire:model="quantity" class="border-emerald-500" />
+        </div>
+
+    </div>
+
+    <x-slot:actions>
+        <x-button label="Cancel" class="" wire:click="$parent.isModalOpen=false" />
+        <x-button label="Save Product"
+            class="text-sm text-white border-none rounded-md bg-emerald-500 hover:bg-emerald-600 active:bg-white"
+            type="submit" spinner="save" />
+    </x-slot:actions>
+
+</x-form>
